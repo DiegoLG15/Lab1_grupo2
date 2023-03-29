dc1f961d0e310623c5f252c741c434f1ffb438dd
DiegoLG15 <a20200839@pucp.edu.pe>
2023-03-28 20:49:52 -0500

    Lab1 Grupo2

---
+++ Lab1_grupo2/src/main/java/com/example/lab1_grupo2/Lab1Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Grupo2Application.class, args);
+	}
+
+}
